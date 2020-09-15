68eaf3786a88e7e59f064bcca9d333b3321b9011
Ashish Vijayaram <initrd@gmail.com>
2020-09-15 15:51:38 -0700

    Fix dependencies, add more tests

--- spec/classes/duo_unix_spec.rb
+++ spec/classes/duo_unix_spec.rb
@@ -1,66 +1,80 @@
 require 'spec_helper'
 
 describe 'duo_unix' do
+  let(:params) do
+    {
+      'ikey'     => 'DIXXXXXXXXXXXXXXXXXX',
+      'skey'     => 'X1hXztPX1rb1X71x1wXkpnmXXvqXXXqqj1XoXbbXu',
+      'host'     => 'api-xxxxxxxx.duosecurity.com',
+      'pushinfo' => 'yes',
+    }
+  end
+
   context 'on supported operating systems' do
     on_supported_os.each do |os, facts|
       context "on #{os}" do
         let(:facts) { facts }
 
-        let(:params) do
-          {
-            'ikey'     => 'DIXXXXXXXXXXXXXXXXXX',
-            'skey'     => 'X1hXztPX1rb1X71x1wXkpnmXXvqXXXqqj1XoXbbXu',
-            'host'     => 'api-xxxxxxxx.duosecurity.com',
-            'pushinfo' => 'yes',
-          }
+        context 'when managing packages' do
+          let(:params) do
+            super().merge({ 'manage_ssh' => true })
+          end
+
+          it { is_expected.to contain_package('openssh-server').with_ensure('installed') }
+
+          case facts[:osfamily]
+          when 'Debian'
+            it { is_expected.to contain_class('duo_unix::apt') }
+            it { is_expected.to contain_package('duo-unix').with_ensure('installed') }
+          when 'RedHat'
+            it { is_expected.to contain_class('duo_unix::yum') }
+            it { is_expected.to contain_yumrepo('duosecurity') }
+            it { is_expected.to contain_yumrepo('duosecurity').that_comes_before('Package[duo_unix]') }
+            it { is_expected.to contain_package('duo_unix').with_ensure('installed') }
+          end
         end
 
-        context 'on usage: pam' do
+        context 'when usage: pam' do
           let(:params) do
-            {
-              'usage' => 'pam',
-            }
+            super().merge({ 'usage' => 'pam' })
           end
 
           it { is_expected.to contain_class('duo_unix::pam') }
-          it {
-            is_expected.to contain_file('/etc/duo/pam_duo.conf').with(
-              'ensure' => 'file',
-            )
-          }
+          it { is_expected.to contain_file('/etc/duo/pam_duo.conf').with_content %r{^host=api-xxxxxxxx.duosecurity.com} }
+          it { is_expected.to contain_augeas('Duo Security SSH Configuration').with_changes(['set /files/etc/ssh/sshd_config/UsePAM yes', 'set /files/etc/ssh/sshd_config/UseDNS no', 'set /files/etc/ssh/sshd_config/ChallengeResponseAuthentication yes']) }
+          it { is_expected.to contain_augeas('PAM Configuration') }
           it { is_expected.to compile.with_all_deps }
         end
 
-        context 'on usage: login' do
+        context 'when usage: login' do
           let(:params) do
-            {
-              'usage' => 'login',
-            }
+            super().merge({ 'usage' => 'login' })
           end
 
           it { is_expected.to contain_class('duo_unix::login') }
           it {
             is_expected.to contain_file('/etc/duo/login_duo.conf').with(
-              'ensure' => 'file',
+              'ensure' => 'present',
             )
           }
+          it { is_expected.to contain_augeas('Duo Security SSH Configuration').with_changes(['set /files/etc/ssh/sshd_config/ForceCommand /usr/sbin/login_duo', 'set /files/etc/ssh/sshd_config/PermitTunnel no', 'set /files/etc/ssh/sshd_config/AllowTcpForwarding no']) }
           it { is_expected.to compile.with_all_deps }
         end
       end
     end
   end
 
-  context 'on unsupported operating systems' do
-    let(:facts) do
+  context 'when on an unsupported Operating System' do
+    let (:facts) do
       {
-        'osfamily' => 'Suse',
+        'os' => {
+          'family' => 'MS-DOS',
+        }
       }
     end
 
-    it do
-      expect {
-        catalogue
-      }.to raise_error(Puppet::Error, %r{Module duo_unix does not support})
+    it 'should fail' do
+      expect { is_expected.to compile }.to raise_error(/does not support/)
     end
   end
 end
